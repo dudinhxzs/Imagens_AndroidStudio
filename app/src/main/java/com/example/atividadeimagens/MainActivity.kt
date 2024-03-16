a5ce17e1310b0775292a3ca8ab4b76ad2f7e45b3
Karinne Angelo <kakaangelo25@gmail.com>
2024-03-16 00:27:57 -0300

    First Commit

---
+++ app/src/main/java/com/example/atividadeimagens/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.atividadeimagens
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.atividadeimagens.ui.theme.AtividadeImagensTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AtividadeImagensTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
+                        message = stringResource(R.string.title)
+
+
+                        )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 48.sp,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.digital_learning)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 4.7F,
+            modifier = Modifier.matchParentSize()
+
+        )
+        GreetingText(
+            message = message,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    AtividadeImagensTheme {
+        GreetingImage(
+            message = stringResource(R.string.title)
+
+        )
+    }
+}
